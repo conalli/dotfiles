3534cd86f8ebc612b580e1992f7c4092ba55f994
Conall <conallimlah@gmail.com>
2024-06-02 20:56:57 +0900

    add initial config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+--local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Tokyo Night (Gogh)",
+	font_size = 14.0,
+	window_background_opacity = 0.8,
+	hide_tab_bar_if_only_one_tab = true,
+	window_padding = {
+		top = 0,
+		bottom = 0,
+		right = 0,
+		left = 0,
+	},
+	inactive_pane_hsb = {
+		saturation = 0.8,
+		brightness = 0.7,
+	},
+}
